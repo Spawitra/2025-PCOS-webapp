3c0a4de08f778b9f68e59a9b5b6ef27ed3adfade
Spawitra <76878259+Spawitra@users.noreply.github.com>
2025-08-23 17:01:41 +0700

    Update Pcos-app.py

--- Pcos-app.py
+++ Pcos-app.py
@@ -66,11 +66,13 @@ def predict_risk(age, weight, cycle_ri, cycle_length, hair_growth, skin_dark, pi
 st.title("🧬 PCOS Risk Self-Assessment")
 st.write("ประเมินความเสี่ยงเบื้องต้น (ไม่ใช่การวินิจฉัยจากแพทย์)")
 
-col1, col2 = st.columns(2)
+col1, col2 , col3= st.columns(3)
 
 with col1:
     age = st.number_input("Age (yrs)", min_value=10, max_value=60, value=25)
     weight = st.number_input("Weight (Kg)", min_value=30, max_value=200, value=60)
+with col2:  
+    st.write("ประเมินความเสี่ยงเบื้องต้น (ไม่ใช่การวินิจฉัยจากแพทย์)")
     cycle_ri = st.radio("Cycle (R/I)", ["R", "I"])
     cycle_length = st.number_input("Cycle length (days)", min_value=15, max_value=60, value=28)
     hair_growth = st.radio("Hair growth", ["Y", "N"]) 
@@ -80,12 +82,13 @@ with col1:
     if Skindarken:
         st.image(Skindarken, caption="จุดสังเกตผิวคล้ำ", use_container_width=True)
 
-with col2:
+with col3:
     pimples = st.radio("Pimples", ["Y", "N"])
     fast_food = st.radio("Fast food", ["Y", "N"])
     foll_l = st.number_input("Follicle No. (L)", min_value=0, max_value=30, value=10)
     foll_r = st.number_input("Follicle No. (R)", min_value=0, max_value=30, value=10)
     weight_gain = st.radio("Weight gain", ["Y", "N"])
+
 
 if st.button("🔍 ประเมินความเสี่ยง"):
     risk, prob = predict_risk(age, weight, cycle_ri, cycle_length, hair_growth, skin_dark,
@@ -96,3 +99,4 @@ if st.button("🔍 ประเมินความเสี่ยง"):
 with st.expander("📝 รบกวนทำแบบสอบถามการใช้งานเว็บไซต์"):
     st.write("เพื่อปรับปรุงคุณภาพและประสิทธิภาพของแบบประเมิน กรุณาช่วยตอบแบบสอบถามค่ะ 🙏")
     st.markdown("[👉 กดที่นี่เพื่อตอบแบบสอบถาม](https://forms.gle/u7GK9hvWkpWjJjaD9)")
+
