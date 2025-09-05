ab00c491065bd354c44845047becba1009ee0f65
Spawitra <76878259+Spawitra@users.noreply.github.com>
2025-09-05 11:45:08 +0700

    Update Pcos-app.py

--- Pcos-app.py
+++ Pcos-app.py
@@ -106,36 +106,28 @@ st.title("🧬 PCOS Risk Self-Assessment")
 st.markdown("### ประเมินความเสี่ยงโรคภาวะถุงน้ำในรังไข่หลายใบ ")
 
 with st.container():
-
+    st.markdown('<div class="risk-card">', unsafe_allow_html=True)
     col1, col2, col3= st.columns(3)
-        
-    with col1:   
-            st.markdown('<div class="risk-card">', unsafe_allow_html=True)
-            age = st.number_input("Age (yrs) อายุ ", min_value=10, max_value=60, value=25)
-            weight = st.number_input("Weight (Kg) น้ำหนัก ", min_value=30, max_value=200, value=60)
-            cycle_ri = st.radio("🔄 Cycle รอบเดือนมาก 3-7 วัน  (R ปกติ /Iไม่ปกติ )", ["R", "I"])
-            cycle_length = st.number_input("🗓️ Cycle length (days) ระยะห่างต่อรอบ ", min_value=15, max_value=100, value=28)
-            st.markdown('</div>', unsafe_allow_html=True)            
-    
-    with col2:  
-        st.markdown('<div class="risk-card">', unsafe_allow_html=True)
+    with col1:
+        age = st.number_input("Age (yrs) อายุ ", min_value=10, max_value=60, value=25)
+        weight = st.number_input("Weight (Kg) น้ำหนัก ", min_value=30, max_value=200, value=60)
+        cycle_ri = st.radio("🔄 Cycle รอบเดือนมาก 3-7 วัน  (R ปกติ /Iไม่ปกติ )", ["R", "I"])
+        cycle_length = st.number_input("🗓️ Cycle length (days) ระยะห่างต่อรอบ ", min_value=15, max_value=100, value=28)
+    with col2:
         hair_growth = st.radio("Hair growth มีตามจุดต่างๆ ขนขึ้นมากกว่าเดิมหรือไม่", ["Y", "N"]) 
         if HairG:
             st.image(HairG, caption="Ferriman Hair Growth Chart", use_container_width=True)
         skin_dark = st.radio("🌑 Skin darkening สีผิวตามจุดต่างๆ เข้มขึ้น หรือไม่ ", ["Y", "N"])
         if Skindarken:
             st.image(Skindarken, caption="จุดสังเกตผิวคล้ำ", use_container_width=True)
-            st.markdown('</div>', unsafe_allow_html=True)
-    
-    with col3: 
-        st.markdown('<div class="risk-card">', unsafe_allow_html=True)
+    with col3:         
         pimples = st.radio("Pimples มีสิวเพิ่มมากขึ้นหรือไม่ ", ["Y", "N"])
         fast_food = st.radio("🍔 Fast food รับประทานอาหารที่มีไขมันเยอะหรือไม่ ", ["Y", "N"])
         foll_l = st.number_input("รูขุมขนบนหน้า ซ้าย กว้างขึ้นหรือไม่  ", 0, 30, 10)
         foll_r = st.number_input("รูขุมขนบนหน้า ขวา ", 0, 30, 10)
         weight_gain = st.radio("📈 Weight gain มีน้ำหนักที่เพิ่มมากขึ้นผิดปกติ หรือไม่  ", ["Y", "N"])
-        st.markdown('</div>', unsafe_allow_html=True)
-
+        
+ st.markdown('</div>', unsafe_allow_html=True)  
 
 if st.button("🔍 ประเมินความเสี่ยง"):
     risk, prob = predict_risk(age, weight, cycle_ri, cycle_length, hair_growth, skin_dark,
@@ -183,6 +175,7 @@ if st.button("🔍 ประเมินความเสี่ยง"):
 with st.expander("📝 รบกวนทำแบบสอบถามการใช้งานเว็บไซต์"):
     st.write("เพื่อปรับปรุงคุณภาพและประสิทธิภาพของแบบประเมิน กรุณาช่วยตอบแบบสอบถามค่ะ 🙏")
     st.markdown("[👉 กดที่นี่เพื่อตอบแบบสอบถาม](https://forms.gle/u7GK9hvWkpWjJjaD9)")
+
 
 
 
